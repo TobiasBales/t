1eeaf33d71f9a08a2874fc3d6d1c6f7b2d33bb6e
Tobias Bales <tobiasbales@hey.com>
2020-11-19 09:52:42 +0100

    Install streamlink

--- etc/nix/general.nix
+++ etc/nix/general.nix
@@ -2,4 +2,7 @@
 {
   home.file.".alacritty.yml".source = ./home/alacritty.yml;
   home.file."Library/Application Support/MTMR/items.json".source = ./home/mtmr.json;
+  home.packages = with pkgs; [
+    streamlink
+  ];
 }

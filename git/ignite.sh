c543d98bf9f4188a427708799103e7b192b16840
Tobias Bales <hi@tobiasbales.dev>
2021-02-17 10:27:55 +0100

    Add .gitignore to home directory

--- git/ignite.sh
+++ git/ignite.sh
@@ -8,3 +8,8 @@ if [ ! -L "${HOME}/.gitconfig" ]; then
   echo "git config not found, linking"
   ln -s "${CONFIG_DIRECTORY}/git/.gitconfig" "${HOME}/.gitconfig"
 fi
+
+if [ ! -L "${HOME}/.gitignore" ]; then
+  echo "git ignore not found, linking"
+  ln -s "${CONFIG_DIRECTORY}/git/.gitignore" "${HOME}/.gitignore"
+fi
